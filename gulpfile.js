7457b2ca6094e493037acc13ef0103342514a60c
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-01-28 19:15:26 +0100

    Aside styles

--- gulpfile.js
+++ gulpfile.js
@@ -56,8 +56,8 @@ gulp.task('sass', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['_scss/*.scss', '_scss/modules/*.scss'], ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
