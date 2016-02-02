2975f3955d223b0d83c439d93b196b4afc5079ce
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-02-02 12:02:10 +0100

    Slider View

--- js/app.js
+++ js/app.js
@@ -9,68 +9,91 @@
 
     el: '#sliderView',
 
+    events: {
+      'click .js_slide_navigation li' : 'clickNavigation'
+    },
+
     navTemplate: HandlebarsTemplates['slider'],
 
     initialize: function(settings) {
       var opts = settings && settings.options ? settings.options : {};
       this.options = _.extend({}, this.defaults, opts);
       this.cache();
-      this.initLory();
       this.initNavigation();
+      this.initLory();
     },
 
     cache: function() {
-      this.$slides = this.$el.find('.js_slide');
+      this.$slider = this.$el.find('.js_slider');
+      this.$sliderItems = this.$el.find('.js_slide');
+
+      this.$sliderNavigation = this.$el.find('.js_slide_navigation');
     },
 
     // Slider plugin
     initLory: function() {
       // set width of each element
-      this.el.addEventListener('before.lory.init', this.setSlideWidth.bind(this));
+      this.$slider[0].addEventListener('before.lory.init', this.setSlideWidth.bind(this));
+      this.$slider[0].addEventListener('after.lory.init', this.setNavigation.bind(this));
+      this.$slider[0].addEventListener('after.lory.slide', this.setNavigation.bind(this));
 
       // init slider
-      lory(this.el, {
+      this.slider = lory(this.$slider[0], {
         slidesToScroll: 2,
-        infinite: 2,
-        enableMouseEvents: true
+        infinite: 2
       });
+
+
     },
 
     setSlideWidth: function() {
-      var width = this.$el.width()/ 2;
-      this.$slides.width(width);
+      var width = this.$slider.width()/ 2;
+      this.$sliderItems.width(width);
     },
 
     initNavigation: function() {
-      var lengthCeil = Math.ceil(this.$slides.length/2);
-      console.log(lengthCeil);
+      var pages = Math.ceil(this.$sliderItems.length/2);
+      var arrayPages =(function(a,b){while(a--)b[a]=a+1;return b})(pages,[]);
+
+      this.$sliderNavigation.html(this.navTemplate({pages: arrayPages}));
+      this.$sliderNavigationItems = this.$sliderNavigation.find('li');
+    },
+
+    // Events
+    clickNavigation: function(e) {
+      e && e.preventDefault();
+      var index = $(e.currentTarget).data('index');
+      var direction = $(e.currentTarget).data('direction');
+      if (index != undefined) {
+        this.slider.slideTo(index*2)
+      } else {
+        switch (direction) {
+          case 'left':
+            this.slider.prev();
+          break;
+          case 'right':
+            this.slider.next();
+          break;
+        }
+      }
+      console.log();
+    },
+
+    // Events
+    setNavigation: function(e) {
+      e && e.preventDefault();
+      var current = 0;
+      if (this.slider) {
+        current = Math.ceil(this.slider.returnIndex()/2);
+      }
+      // Set current
+      this.$sliderNavigationItems.removeClass('-active');
+      this.$sliderNavigation.find('li[data-index='+current+']').addClass('-active');
     }
 
   });
 
 })(this);
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-//   var slider = document.querySelector('.js_slider');
-
-//   slider.addEventListener('before.lory.init', setSlideWidth);
-
-//   lory(slider, {
-//     slidesToScroll: 2,
-//     infinite: 2,
-//     enableMouseEvents: true
-//   });
-
-// });
-
-// function setSlideWidth() {
-//  var $slider = $('.js_slider');
-//  var $slide = $('.js_slide');
-//  var width = $slider.width()/ 2;
-//  $slide.width(width);
-// }
-
 
 (function(root) {
 
--- js/templates.js
+++ js/templates.js
@@ -1,8 +1,16 @@
 this["HandlebarsTemplates"] = this["HandlebarsTemplates"] || {};
-this["HandlebarsTemplates"]["slider"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+this["HandlebarsTemplates"]["slider"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=this.escapeExpression;
 
-  return "<div>\n  "
-    + this.escapeExpression(((helper = (helper = helpers.test || (depth0 != null ? depth0.test : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"test","hash":{},"data":data}) : helper)))
-    + "\n</div>\n";
+  return "    <li class=\"item\" data-index=\""
+    + alias1(((helper = (helper = helpers.index || (data && data.index)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"index","hash":{},"data":data}) : helper)))
+    + "\">"
+    + alias1(this.lambda(depth0, depth0))
+    + "</li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return "\n<ul>\n  <li class=\"item -arrow\" data-direction=\"left\"><svg><use xlink:href=\"#icon-arrowleft\"></use></svg></li>\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "  <li class=\"item -arrow\" data-direction=\"right\"><svg><use xlink:href=\"#icon-arrowright\"></use></svg></li>\n</ul>\n\n";
 },"useData":true});
