e427791fb57a0b9aeb5a82ec6f375273a1acafe2
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2016-02-15 21:47:37 +0100

    Some slider amendments

--- assets/js/views/slider.js
+++ assets/js/views/slider.js
@@ -11,15 +11,30 @@
 
     events: {
       'click .js_slide_navigation li' : 'clickNavigation'
-    },
+    }, 
 
     navTemplate: HandlebarsTemplates['slider'],
 
     initialize: function(settings) {
       var opts = settings && settings.options ? settings.options : {};
       this.options = _.extend({}, this.defaults, opts);
-      this.options.slides_per_slide = ($(window).width() < 850) ? 1 : 2;
-      this.cache();
+
+      enquire.register("screen and (min-width: 850px)", {
+        match: function(){
+          this.mobile = false;
+          this.initSlider();
+        }.bind(this)
+      });
+      enquire.register("screen and (max-width: 850px)", {
+        match: function(){
+          this.mobile = true;
+          this.initSlider();
+        }.bind(this)
+      });
+    },
+
+    initSlider: function() {
+      this.options.slider = this.setOptions();
       this.initNavigation();
       this.initLory();
     },
@@ -27,36 +42,44 @@
     cache: function() {
       this.$slider = this.$el.find('.js_slider');
       this.$sliderItems = this.$el.find('.js_slide');
+      this.slideCount = this.$el.find('.js_slide').length;
 
       this.$sliderNavigation = this.$el.find('.js_slide_navigation');
     },
 
     // Slider plugin
     initLory: function() {
-      this.options.slider = this.setOptions();
+      // init slider
+      if (!!this.slider) {
+        this.slider.reset();
+        this.slider.destroy();
+      }
+
       // set width of each element
       this.$slider[0].addEventListener('before.lory.init', this.setSlideWidth.bind(this));
+      this.$slider[0].addEventListener('on.lory.resize', this.setSlideWidth.bind(this));
       this.$slider[0].addEventListener('after.lory.init', this.setNavigation.bind(this));
       this.$slider[0].addEventListener('after.lory.slide', this.setNavigation.bind(this));
 
-      // init slider
       this.slider = lory(this.$slider[0], this.options.slider);
     },
 
     setOptions: function() {
+      this.cache();
       return {
-        slidesToScroll: this.options.slides_per_slide,
-        infinite: this.options.slides_per_slide,
+        slidesToScroll: (!!this.mobile) ? 1 : 2,
+        infinite: (!!this.mobile) ? 1 : 2,
+        slides_per_slide: (!!this.mobile) ? 1 : 2
       }
     },
 
     setSlideWidth: function() {
-      var width = this.$slider.width()/this.options.slides_per_slide;
+      var width = this.$slider.width()/this.options.slider.slides_per_slide;
       this.$sliderItems.width(width);
     },
 
     initNavigation: function() {
-      var pages = Math.ceil(this.$sliderItems.length/this.options.slides_per_slide);
+      var pages = Math.ceil(this.slideCount/this.options.slider.slides_per_slide);
       var arrayPages =(function(a,b){while(a--)b[a]=a+1;return b})(pages,[]);
 
       this.$sliderNavigation.html(this.navTemplate({pages: arrayPages}));
@@ -69,7 +92,7 @@
       var index = $(e.currentTarget).data('index');
       var direction = $(e.currentTarget).data('direction');
       if (index != undefined) {
-        this.slider.slideTo(index*this.options.slides_per_slide)
+        this.slider.slideTo(index*this.options.slider.slides_per_slide)
       } else {
         switch (direction) {
           case 'left':
@@ -89,7 +112,6 @@
       if (this.slider) {
         current = Math.ceil(this.slider.returnIndex()/2);
       }
-      console.log(current);
       // Set current
       this.$sliderNavigationItems.removeClass('-active');
       this.$sliderNavigation.find('li[data-index='+current+']').addClass('-active');
